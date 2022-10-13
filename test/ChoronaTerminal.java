c22f12d5d029b1d3ae1c50522ef35a1c33929855
Fezaaan <106581271+Fezaaan@users.noreply.github.com>
2022-10-13 13:50:55 +0200

    Update ChoronaTerminal.java
    Shit whack

--- test/ChoronaTerminal.java
+++ test/ChoronaTerminal.java
@@ -6,6 +6,35 @@ import java.awt.*;
 public class ChoronaTerminal extends JFrame {
     public ChoronaTerminal(Variant key, Room room) {
         this.setTitle("CHORona - "+ key.getLabel()+"("+key.getDesignation()+")");
+<<<<<<< Updated upstream
+=======
+        this.setLayout(new BorderLayout());
+
+        //top--------------
+        int stepNumber = 0;
+        JLabel step = new JLabel("Steps: "+ room.getSteps());
+        JPanel top = new JPanel();
+        top.add(step);
+        this.add(top,BorderLayout.NORTH);
+        //middle-----------
+        JPanel middle = new JPanel(new GridLayout(room.getSetting().getHeight(),room.getSetting().getWidth()));
+        int felder = room.getSetting().getHeight()*room.getSetting().getWidth();
+        for(int i =0; i<felder; i++){
+            middle.add(new CellButton());
+        }
+        this.add(middle, BorderLayout.CENTER);
+        //bottom
+        JPanel bot = new JPanel(new GridLayout(1,3,20,20));
+        JButton stepButton = new JButton("Step");
+        bot.add(stepButton);
+        JButton playButton = new JButton("Play");
+        bot.add(playButton);
+        JButton saveButton = new JButton("Save");
+        bot.add(saveButton);
+        this.add(bot, BorderLayout.SOUTH);
+    //def
+        //this.pack();
+>>>>>>> Stashed changes
         this.setSize(640,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
